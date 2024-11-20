45f759c0ffe495027b7ce7eb6c020cf89de352bd
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-11-20 15:21:10 +0100

    Initial Commit

---
+++ ComarchCwiczeniaTesty/ComarchCwiczeniaTesty/CalculatorService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComarchCwiczeniaTesty
+{
+    public class CalculatorService
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        
+        public float Dividy(int x, int y)
+        {
+            return x / (float)y;
+        }
+    }
+}
---
+++ ComarchCwiczeniaTesty/Tests/ComarchCwiczeniaTesty.UnitTests/CalculatorServiceTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComarchCwiczeniaTesty.UnitTests
+{
+
+    [TestFixture]
+    public class CalculatorServiceTests
+    {
+        CalculatorService cut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            cut = new CalculatorService();
+        }
+
+
+        [TearDown]
+        public void Teardown()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+
+        [Test]
+        public void AddShouldReturnCorrectSumValue()
+        {
+            // Arrange
+            int x = 2, y = 2;
+            int expected = 4;
+            int actual = 0;
+
+            // Act
+            actual = cut.Add(x, y);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void SubtractShouldReturnCorrectValue()
+        {
+            // Arrange
+            int x = 2, y = 2;
+            int expected = 0;
+            int actual = 0;
+
+            // Act
+            actual = cut.Subtract(x, y);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void MultiplyShouldReturnCorrectValue()
+        {
+            // Arrange
+            int x = 2, y = 2;
+            int expected = 4;
+            int actual = 4;
+
+            // Act
+            actual = cut.Multiply(x, y);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void DividyShouldReturnCorrectValue()
+        {
+            // Arrange
+            int x = 2, y = 2;
+            float expected = 1;
+            float actual = 0;
+
+            // Act
+            actual = cut.Dividy(x, y);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+
+    }
+}
