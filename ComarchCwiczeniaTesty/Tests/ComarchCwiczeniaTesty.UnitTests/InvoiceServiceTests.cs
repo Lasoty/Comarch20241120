b044124ff0ca70e887f7f9bc0866a7f170989f12
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-11-21 15:53:33 +0100

    Moq Setup Callbacks and Throws

--- ComarchCwiczeniaTesty/Tests/ComarchCwiczeniaTesty.UnitTests/InvoiceServiceTests.cs
+++ ComarchCwiczeniaTesty/Tests/ComarchCwiczeniaTesty.UnitTests/InvoiceServiceTests.cs
@@ -71,4 +71,63 @@ internal class InvoiceServiceTests
         // Assert
         actual.Should().Be(95m);
     }
+
+    [Test]
+    public void CalculateTotalShouldThrowsExceptionWhenTaxServiceFails()
+    {
+        // Arrange
+        var amount = 100m;
+        var customerType = "Regular";
+
+        discountServiceMock.Setup(x => x.CalculateDiscount(It.IsAny<decimal>(), It.IsAny<string>())).Returns(10m);
+        taxServiceMock.Setup(x => x.GetTax(It.IsInRange(decimal.MinValue, 0, Moq.Range.Inclusive)))
+            .Throws(new Exception("Tax calculation failed"));
+
+        // Act
+        Action act = () => invoiceService.CalculateTotal(-1, "");
+
+        // Assert
+        act.Should().Throw<Exception>().WithMessage("Tax calculation failed");
+    }
+
+    [Test]
+    public void CalculateTotalShouldCallsServicesInSequence()
+    {
+        //Arrange
+        var sequence = new MockSequence();
+
+        discountServiceMock.InSequence(sequence)
+            .Setup(x => x.CalculateDiscount(It.IsAny<decimal>(), It.IsAny<string>())).Returns(10m);
+
+        taxServiceMock.InSequence(sequence)
+            .Setup(x => x.GetTax(It.IsAny<decimal>())).Returns(5m);
+
+        // Act
+        var actual = invoiceService.CalculateTotal(100, "");
+
+        //Assert
+        actual.Should().Be(95);
+    }
+
+    [Test]
+    public void CalculateTotalShouldUseGetTaxCallback()
+    {
+        // Arrange
+
+        var amount = 100m;
+        var customerType = "Regular";
+        var callbackParameter = 0m;
+
+        discountServiceMock.Setup(x => x.CalculateDiscount(It.IsAny<decimal>(), It.IsAny<string>())).Returns(10m);
+
+        taxServiceMock.Setup(x => x.GetTax(It.IsAny<decimal>())).Returns(5m)
+            .Callback<decimal>(p => callbackParameter = p);
+
+        // Act
+
+        invoiceService.CalculateTotal(amount, customerType);
+
+        // Assert
+        callbackParameter.Should().Be(amount - 10m);
+    }
 }
