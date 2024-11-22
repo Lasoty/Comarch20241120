f48138f1cb98bea0778ccb1d7dea81b840741fb0
leszek.lewandowski@dnv.com <leszek.lewandowski@dnv.com>
2024-11-22 11:18:50 +0100

    Checkboxes, Dropdowns

--- ComarchCwiczeniaTesty/Tests/ComarchCwiczeniaTesty.E2eTests/SeleniumTests.cs
+++ ComarchCwiczeniaTesty/Tests/ComarchCwiczeniaTesty.E2eTests/SeleniumTests.cs
@@ -1,5 +1,7 @@
+using FluentAssertions;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace ComarchCwiczeniaTesty.E2eTests;
 
@@ -48,4 +50,49 @@ public class SeleniumTests
         var successMessage = driver.FindElement(By.Id("flash"));
         Assert.That(successMessage.Text, Does.Contain("You logged into a secure area!"));
     }
+
+    [Test]
+    public void IncorrectLoginTest()
+    {
+        driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+        var usernameField = driver.FindElement(By.Id("username"));
+        var passwordField = driver.FindElement(By.Id("password"));
+        
+        usernameField.SendKeys("wrongUser");
+        passwordField.SendKeys("wrongPassword");
+        
+        var loginButton = driver.FindElement(By.CssSelector("button[type='submit']"));
+        loginButton.Click();
+        
+        var errorMessage = driver.FindElement(By.CssSelector(".flash.error"));
+        Assert.That(errorMessage.Text, Does.Contain("Your username is invalid!"), "Niepoprawne dane logowania nie wywo³a³y b³êdu.");
+    }
+
+    [Test]
+    public void CheckboxesTest()
+    {
+        driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/checkboxes");
+
+        var checkbox = driver.FindElement(By.XPath("//*[@id=\"checkboxes\"]/input[2]"));
+        bool isSelected = checkbox.Selected;
+
+        isSelected.Should().BeTrue("Checkbox powinien byæ zaznaczony");
+    }
+
+    [Test]
+    public void DropDownTest()
+    {
+        driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/dropdown");
+
+        var dropdown = driver.FindElement(By.Id("dropdown"));
+
+        var selectElement = new SelectElement(dropdown);
+        selectElement.SelectByValue("1");
+
+        selectElement.SelectedOption.Text.Should().Contain("Option 1");
+
+    }
+
+
 }
